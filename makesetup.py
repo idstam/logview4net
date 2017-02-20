610f41fb1ad2b33c429ff177b0b173fb551181c3
Johan Idstam <johan.idstam@adlibris.com>
2017-02-20 14:21:17 +0100

    Making the python script a bit more readable.

--- makesetup.py
+++ makesetup.py
@@ -5,17 +5,51 @@ import xml.dom.minidom
 import codecs
 from subprocess import call
 
+def main():
+    """ This is the main method. """
+    long_version = get_new_version_long_form()
+    short_version = get_new_version_short_form()
+    old_version = get_old_version_from_SolutionVersionInfo()
+    replace_solution_version(old_version, long_version)
 
-def sign_exe(sign_tool_path, cert_path, password, version_info,  exe_file,  sign_log_file):
+    msbuild_path = r"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe"
+    nsis_path = r"C:\Program Files (x86)\NSIS\makensis"
+    sign_tool_path = r"C:\Program Files (x86)\Windows Kits\8.1\bin\x86\SignTool.exe"
+    cert_path = r"X:\ComodoCodeSigningCert.pfx"
+    password = input("Please enter code signing password:")
+    version_info = "logview4net " + long_version
+    exe_file = r".\src\App\bin\release\logview4net.exe"
+    sign_log_file = "sign_log.txt"
+
+    clean_solution()
+    build_solution(msbuild_path)
+    sign_exe(sign_tool_path, cert_path, password, version_info, exe_file, sign_log_file)
+    make_installer(nsis_path)
+
+    version_info = "logview4net " + long_version + " installer"
+    exe_file = r".\src\setup\logview4net_setup.exe"
+    sign_exe(sign_tool_path, cert_path, password, version_info, exe_file, sign_log_file)
+
+    write_auto_update_version(short_version)
+    update_pad()
+
+    copy_files_to_site()
+
+    input("Press Enter to continue...")
+
+
+def sign_exe(sign_tool_path, cert_path, password, version_info, exe_file, sign_log_file):
     print("Signing " + exe_file)
 
-    sign_command = r'"%s" sign /f "%s" /p "%s" /d "%s" /v /tr http://timestamp.comodoca.com "%s"' % (sign_tool_path, cert_path, password, version_info,  exe_file) #,  sign_log_file)
+    sign_command = r'"%s" sign /f "%s" /p "%s" /d "%s" /v /tr http://timestamp.comodoca.com "%s"' % (sign_tool_path, cert_path, password, version_info, exe_file) #,  sign_log_file)
 
     #sign_command = r'"%s" ' % (sign_tool_path)
     print(sign_command)
     call(sign_command)
 
+
 def make_installer(nsis_path):
+    """ Create the Nsis installer package. """
     print("Making installer")
     call(nsis_path + r" .\src\setup\logview4net.nsi")
 
@@ -32,10 +66,10 @@ def build_solution(msbuild_path):
 
 def write_auto_update_version(version):
     version_file = r".\src\Deployment\logview4net.version"
-    write_auto_update_version_file(version, version_file):
+    write_auto_update_version_file(version, version_file)
 
     version_file = r".\site_hugo\static\dlfolder\logview4net.version"
-    write_auto_update_version_file(version, version_file):
+    write_auto_update_version_file(version, version_file)
 
 def write_auto_update_version_file(version, version_file):
     print("Creating version file " + version_file)
@@ -60,7 +94,7 @@ def replace_solution_version(old_version, new_version):
     delete_file(old_solution_file)
     delete_file(tmp_file)
 
-    f1 = open( solution_file, "r" )
+    f1 = open( solution_file, "r")
     f2 = open('tmp.cs', 'w')
     for line in f1:
         f2.write(line.replace(old_version, new_version))
@@ -153,34 +187,5 @@ def delete_folder(folder):
     except OSError:
         pass
 
-##############################################################
-
-long_version = get_new_version_long_form()
-short_version = get_new_version_short_form()
-old_version = get_old_version_from_SolutionVersionInfo()
-replace_solution_version(old_version, long_version)
-
-msbuild_path = r"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe"
-nsis_path = r"C:\Program Files (x86)\NSIS\makensis"
-sign_tool_path = r"C:\Program Files (x86)\Windows Kits\8.1\bin\x86\SignTool.exe"
-cert_path = r"X:\ComodoCodeSigningCert.pfx"
-password = input("Please enter code signing password:")
-version_info = "logview4net " + long_version
-exe_file = r".\src\App\bin\release\logview4net.exe"
-sign_log_file = "sign_log.txt"
-
-clean_solution()
-build_solution(msbuild_path)
-sign_exe(sign_tool_path, cert_path, password, version_info,  exe_file,  sign_log_file)
-make_installer(nsis_path)
-
-version_info = "logview4net " + long_version + " installer"
-exe_file = r".\src\setup\logview4net_setup.exe"
-sign_exe(sign_tool_path, cert_path, password, version_info,  exe_file,  sign_log_file)
-
-write_auto_update_version(short_version)
-update_pad()
-
-copy_files_to_site()
-
-input("Press Enter to continue...")
+###########################################################################################
+main()
