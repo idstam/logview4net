e8c08b771af377937745db0a922eba70643c633d
Johan Idstam <johan.idstam@adlibris.com>
2017-02-21 11:04:21 +0100

    Change msbuild version and remove code signing from build script

--- makesetup.py
+++ makesetup.py
@@ -4,6 +4,7 @@ import shutil
 import xml.dom.minidom
 import codecs
 from subprocess import call
+import subprocess
 
 def main():
     """ This is the main method. """
@@ -12,23 +13,17 @@ def main():
     old_version = get_old_version_from_SolutionVersionInfo()
     replace_solution_version(old_version, long_version)
 
-    msbuild_path = r"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe"
+    msbuild_path = r"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
     nsis_path = r"C:\Program Files (x86)\NSIS\makensis"
-    sign_tool_path = r"C:\Program Files (x86)\Windows Kits\8.1\bin\x86\SignTool.exe"
-    cert_path = r"X:\ComodoCodeSigningCert.pfx"
-    password = input("Please enter code signing password:")
-    version_info = "logview4net " + long_version
     exe_file = r".\src\App\bin\release\logview4net.exe"
-    sign_log_file = "sign_log.txt"
 
     clean_solution()
     build_solution(msbuild_path)
-    sign_exe(sign_tool_path, cert_path, password, version_info, exe_file, sign_log_file)
+    make_file_hash(exe_file)
     make_installer(nsis_path)
 
-    version_info = "logview4net " + long_version + " installer"
     exe_file = r".\src\setup\logview4net_setup.exe"
-    sign_exe(sign_tool_path, cert_path, password, version_info, exe_file, sign_log_file)
+    make_file_hash(exe_file)
 
     write_auto_update_version(short_version)
     update_pad()
@@ -37,15 +32,12 @@ def main():
 
     input("Press Enter to continue...")
 
+def make_file_hash(source):
+    """Calculate the Sha256 hash of a file. """
 
-def sign_exe(sign_tool_path, cert_path, password, version_info, exe_file, sign_log_file):
-    print("Signing " + exe_file)
-
-    sign_command = r'"%s" sign /f "%s" /p "%s" /d "%s" /v /tr http://timestamp.comodoca.com "%s"' % (sign_tool_path, cert_path, password, version_info, exe_file) #,  sign_log_file)
-
-    #sign_command = r'"%s" ' % (sign_tool_path)
-    print(sign_command)
-    call(sign_command)
+    output = subprocess.Popen(['certutil', "-hashfile", source, "sha256"], stdout=subprocess.PIPE).communicate()[0]
+    hash_file = open(source + '.sha256', 'wb')
+    hash_file.write(output)
 
 
 def make_installer(nsis_path):
@@ -177,8 +169,6 @@ def delete_file(file):
 
 def copy_files_to_site():
     shutil.copyfile(r".\src\setup\logview4net_setup.exe", r".\site_hugo\static\dlfolder\logview4net_setup.exe")
-    shutil.copyfile(r".\src\Deployment\logview4net.version", r".\site_hugo\static\dlfolder\logview4net.version")
-    shutil.copyfile(r".\src\Deployment\logview4net.pad.xml", r".\site_hugo\static\dlfolder\logview4net.pad.xml")
 
 
 def delete_folder(folder):
